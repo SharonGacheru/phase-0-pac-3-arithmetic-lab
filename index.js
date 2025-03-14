c28651b36f7ecafb2b1e52a8cd6f5a5a7424823c
Sharon Gacheru <sharongacheru2@gmail.com>
2025-03-14 13:57:57 +0300

    JavaScript Calculator

---
+++ index.js
@@ -0,0 +1,34 @@
+// Basic Math Functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Math + Assignment Functions
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Parsing Numbers Functions
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
